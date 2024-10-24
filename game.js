f176fca9d95dec0455744066d1d80c2f2310a9eb
TheMiner11235 <110794508+TheMiner11235@users.noreply.github.com>
2024-10-24 11:36:47 -0700

    Update game.js

--- game.js
+++ game.js
@@ -2,8 +2,8 @@ let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d");
 
 // Desired base resolution for the game
-const baseWidth = 790;
-const baseHeight = 843;
+const baseWidth = 800;
+const baseHeight = 800;
 
 // Snake segment size
 const box = 20;
@@ -12,8 +12,8 @@ const box = 20;
 const maxSnakeLength = Math.floor((baseWidth * baseHeight) / (box * box));
 
 // New maximum height for apple and snake
-const maxHeight = 800;
-
+const maxHeight = 843;
+const maxWidth = 790;
 let frameRate = 60;
 let snakeSpeed = 15;
 let timeSinceLastMove = 0;
